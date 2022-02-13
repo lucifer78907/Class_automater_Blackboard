f488698e4c8bec97858d4fd8d35620d849f43586
Rudra <chaudhary66667@gmail.com>
2022-02-13 15:51:45 +0530

    +explict-wait for proxy pop-up

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 from dotenv import load_dotenv
 from os import getenv
 
 load_dotenv()
 CHROMEDRIVER_EXEC_PATH = Service(f'{getenv("CHROME_DRIVER_PATH")}')
+Website = f'{getenv("CLASSES_WEBSITE")}'
 
 driver = webdriver.Chrome(service=CHROMEDRIVER_EXEC_PATH)
-driver.get("https://www.google.com")
+driver.get(f"{Website}")
 driver.maximize_window()
-time.sleep(5)
+try:
+    element = WebDriverWait(driver,5).until(EC.presence_of_element_located((By.CLASS_NAME,"button-1")))
+    agree_button = driver.find_element(by=By.CLASS_NAME,value="button-1")
+    agree_button.click()
+except Exception as E:
+    print('Failed')
+time.sleep(100)
 driver.close()
