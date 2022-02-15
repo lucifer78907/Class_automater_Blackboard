b91eb45d9df3a4d80eab44a9c136bb9c145e0d16
Rudra <chaudhary66667@gmail.com>
2022-02-15 14:29:59 +0530

    +cam,mic access +default_profile_launch

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ from selenium.webdriver.support import expected_conditions as EC  # to specify t
 from selenium.webdriver.common.by import By  # to find elements By (id,class etc)
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
 import time
 from dotenv import load_dotenv
 from os import getenv
@@ -14,9 +15,14 @@ CHROMEDRIVER_EXEC_PATH = Service(f'{getenv("CHROME_DRIVER_PATH")}')
 Website = f'{getenv("CLASSES_WEBSITE")}'
 user_id = f'{getenv("USER_ID")}'
 password = f'{getenv("PASSWORD")}'
-test_link = 'https://cuchd.blackboard.com/ultra/courses/_54496_1/outline'
-
-driver = webdriver.Chrome(service=CHROMEDRIVER_EXEC_PATH)
+test_link = 'https://cuchd.blackboard.com/ultra/courses/_49461_1/outline'
+op = Options()
+op.add_argument("user-data-dir=/home/rudra/.config/google-chrome")
+op.add_experimental_option("prefs", {
+    "profile.default_content_setting_values.media_stream_mic": 1, 
+    "profile.default_content_setting_values.media_stream_camera": 1,
+  })
+driver = webdriver.Chrome(service=CHROMEDRIVER_EXEC_PATH,options=op)
 driver.get(f"{Website}")
 driver.maximize_window()
 try:
@@ -31,7 +37,7 @@ try:
     password_input.send_keys(password)
     password_input.send_keys(Keys.ENTER)
     driver.get(test_link)
-    drop_down_ele = WebDriverWait(driver, 5).until(EC.element_to_be_clickable((By.ID, "sessions-list-dropdown")))
+    drop_down_ele = WebDriverWait(driver, 9).until(EC.element_to_be_clickable((By.ID, "sessions-list-dropdown")))
     # time.sleep(5)
     join_session_drop_down = driver.find_element(by=By.ID,value="sessions-list-dropdown")
     join_session_drop_down.click()
